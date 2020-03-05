826a2961396b1d4a57a172286e7639a92909c8a5
zahidul islam <zahidulislam5594@gmail.com>
2020-03-06 01:40:29 +0600

    Add project files.

---
+++ AllConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "We live in an age of Science";
+            
+            Console.WriteLine(String.Concat(str.Where(c => !char.IsWhiteSpace(c))).ToLower());
+        }
+    }
+}
